aac6d580b643e84d54cba1249f43eccc43deb76a
mattak <mattak.me@gmail.com>
2025-02-17 12:28:34 +0900

    :seedling: add release tag & release page script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,7 @@ echo "Version: ${VERSION}"
 TARGET_DIR="chrome-githubNewlineReplacer-v${VERSION}"
 TARGET_ZIP="chrome-githubNewlineReplacer-v${VERSION}.zip"
 
+# ビルドとzipファイルの作成
 rm -rf dist/ 
 tsc
 cp src/manifest.json dist/ 
@@ -17,3 +18,15 @@ zip -r ${TARGET_ZIP} ${TARGET_DIR}
 rm -rf ${TARGET_DIR}
 
 echo "Created: ${TARGET_ZIP}"
+
+# タグの作成と push
+git tag "v${VERSION}"
+git push origin "v${VERSION}"
+
+# GitHub Release の作成
+gh release create "v${VERSION}" \
+  --title "v${VERSION}" \
+  --notes "Release v${VERSION}" \
+  "${TARGET_ZIP}"
+
+echo "Created GitHub Release: v${VERSION}"
